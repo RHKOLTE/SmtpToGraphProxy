44b5a6070d2b7f481a03d95159417d24943a6dae
RHKOLTE <rhkolte@gmail.com>
2025-06-16 17:15:43 +0530

    Update Send-MailMessageSMTP.ps1

--- test/Send-MailMessageSMTP.ps1
+++ test/Send-MailMessageSMTP.ps1
@@ -14,12 +14,12 @@ $attachments = @("C:\test\dummy.pdf", "C:\test\dummy1.pdf")
 Send-MailMessage `
     -From "gl-notification@greenlightcorp.com" `
     -To "user1@xyzcorp.com" `
-	-Cc "user1@gmail.com" `
-	-Bcc "user12@gmail.com" `
+    -Cc "user1@gmail.com" `
+    -Bcc "user12@gmail.com" `
     -Subject "Test Email from PowerShell" `
     -Body "This is a test email." `
     -SmtpServer $smtpServer `
     -Port $smtpPort `
-	-Attachments $attachments `
-	-UseSsl `
+    -Attachments $attachments `
+    -UseSsl `
     -Credential $cred
